382d914745c70021c732e3572c67f3b2a6ab20ab
tdvance <tdvance@gmail.com>
2016-09-29 15:10:49 -0400

    Player can now respawn
    spawns to one of (currently) 9 locations, which of the nine is chosen
    randomly
    added the nine spawn points to the map by hand so spawning is in
    reasonable locations
    slowed zombie down to make it more zombie-like

---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public GameObject spawnPointsParent;
+    public bool respawn = false;
+
+    // Use this for initialization
+    void Start() {
+        ReSpawn();//start at random spawn point
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (respawn) {
+            ReSpawn();
+            respawn = false;
+        }
+    }
+
+    void ReSpawn() {
+        int index = (int)Random.Range(0, spawnPointsParent.transform.childCount);
+        Transform spawnPoint = spawnPointsParent.transform.GetChild(index);
+
+        transform.position = spawnPoint.position;
+        transform.rotation = spawnPoint.rotation;
+    }
+}
