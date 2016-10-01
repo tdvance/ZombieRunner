b2f98e0f49bc786a69b223d100e31733398cd048
tdvance <tdvance@gmail.com>
2016-09-30 20:05:23 -0400

    add helicopter landing area detection

---
+++ Assets/Scripts/ClearAreaDetector.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClearAreaDetector : MonoBehaviour {
+    public AudioClip clip;
+
+    public float timeSinceLastTrigger = 0;
+    public bool clearAreaFound = false;
+    Vector3 clearAreaLocation;
+    int enterCount = 0;
+
+    // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (enterCount > 0) {
+            timeSinceLastTrigger = 0;
+        }
+        if (!clearAreaFound && timeSinceLastTrigger > 1) {
+            clearAreaFound = true;
+            clearAreaLocation = transform.position;
+            AudioSource.PlayClipAtPoint(clip, clearAreaLocation);
+        } else {
+            timeSinceLastTrigger += Time.deltaTime;
+        }
+    }
+
+    void OnTriggerEnter(Collider collider) {
+        timeSinceLastTrigger = 0;
+        enterCount++;
+    }
+
+    void OnTriggerExit(Collider collider) {
+        enterCount--;
+    }
+
+}
---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DayNightCycle : MonoBehaviour {
+
+    private float sunXangle;
+    private float sunYangle;
+    private float sunZangle;
+
+    private Quaternion startRotation;
+
+    [Tooltip("Set number of minutes that pass per second")]
+    public float timeScale = 1f;
+
+
+    // Use this for initialization
+    void Start() {
+        startRotation = transform.rotation;
+        sunXangle = transform.rotation.eulerAngles.x;
+        sunYangle = transform.rotation.eulerAngles.y;
+        sunZangle = transform.rotation.eulerAngles.z;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float angle = Time.deltaTime / 60 * timeScale;
+        transform.Rotate(Vector3.right, angle);
+    }
+}
